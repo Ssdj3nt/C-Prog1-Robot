c09fa64e45ffea735c535e492ea1ff52d498d3ae
francesco <francescopo@outlook.com>
2022-02-11 14:32:55 +0100

    Revisione

--- main.c
+++ main.c
@@ -1,106 +1,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define  size 16 // Dimensione delle righe e delle colonne.
+#define n 16
+#define m 16
 
 struct data
 {
     int x;
     int y;
 }r;
-
-void visualizza_matrice(char [][size]); // Visualizza in output la stanza.
-void posizione_robot(char [][size]);
-void movimento1(char [][size]);
-void movimento2(char [][size]);
-int *p;
+struct point
+{
+    int p;
+}pre;
+void visualizza_matrice(char [][m]); // Visualizza in output la stanza.
+void posizione_robot(char [][m]);
+void movimento1(char [][m]);
+void movimento2(char [][m]);
+char swap(char *,char *);
 int main()
 {
-    char stanza[size][size]={{'X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X'},
-                                  {'X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ','X',' ',' ',' ','X','X','X','X','X','X','X','X','X'},
-                                  {'X',' ',' ','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ','X',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ','X',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ',' ',' ',' ',' ','X',' ',' ',' ','X','X','X',' ','X'},
-                                  {' ',' ',' ',' ',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {' ',' ',' ',' ',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {' ',' ',' ',' ',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X',' ',' ',' ',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','X'},
-                                  {'X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X'}};
+    char stanza[n][m]={{'X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X'},
+                       {'X','.','.','.','.','.','.','.','.','.','.','.','.','.','.','X'},
+                       {'X','.',' ','X',' ','.',' ',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+                       {'X','.',' ','X',' ','.',' ','X','X','X','X','X','X','X','X','X'},
+                       {'X','.',' ','X',' ','.','.','.','.','.','.','.','.',' ',' ','X'},
+                       {'X','.',' ','X',' ','.',' ',' ',' ',' ',' ',' ','.',' ',' ','X'},
+                       {'X','.',' ','X',' ','.',' ',' ',' ',' ',' ',' ','.',' ',' ','X'},
+                       {'X','.',' ','X',' ','.','.','.','.','.',' ',' ','.',' ',' ','X'},
+                       {'X','.',' ','X',' ','.',' ','X',' ','.',' ',' ','.',' ',' ','X'},
+                       {'X','.',' ','X',' ','.',' ','X',' ','.',' ',' ','.',' ',' ','X'},
+                       {'X','.',' ',' ',' ','.',' ','X',' ','.',' ','X','X','X',' ','X'},
+                       {'-','.',' ',' ',' ','.',' ','X',' ','.','.','.','.',' ',' ','X'},
+                       {'-','.','.','.','.','.',' ','X',' ',' ',' ',' ','.',' ',' ','X'},
+                       {'-','.',' ',' ',' ',' ',' ','X',' ',' ',' ',' ','.',' ',' ','X'},
+                       {'X',' ',' ',' ',' ',' ',' ','X',' ',' ',' ',' ','.',' ',' ','X'},
+                       {'X','X','X','X','X','X','X','X','X','X','X','X','X','X','X','X'}};
     int nc;
     posizione_robot(stanza);
+    visualizza_matrice(stanza);
     srand((unsigned int) time(0));
-
-    while((r.x>=0 && r.x<size-2) && (r.y>=0 && r.y<size-2)) //ciclo ripetuto finchè? il robot resta all'interno alla stanza
+    while((r.x>=0 && r.x<n) && (r.y>=0 && r.y<m))//ciclo ripetuto finch� il robot resta all'interno alla stanza
     {
         visualizza_matrice(stanza);
         printf("RIGA=%d\t COLONNA=%d\n",r.x,r.y);
+        nc=rand()%10;
         movimento1(stanza);
-        /*nc=rand()%10;
-        if(nc>=0 && nc<3)
-            movimento1(stanza);
+        /*if(nc>=0 && nc<3)
+            movimento_casuale(stanza);
         else
-            movimento2(stanza);*/
+            movimento_intelligente(stanza);
+        system("PAUSE");*/
     }
     printf("\nIl robot e' uscito dalla stanza\n");
     visualizza_matrice(stanza);
 }
 
-void visualizza_matrice(char stanza[size][size])
+void visualizza_matrice(char stanza[n][m])
 {
     int i,j;
-    for(i=0;i<size;i++)
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<size;j++)
+        for(j=0;j<m;j++)
             printf("%2c",stanza[i][j]);
         printf("\n");
     }
 }
-void posizione_robot(char stanza[size][size])
+void posizione_robot(char stanza[n][m])
 {
     do
     {
         puts("\n-Indica le cordinate della posizione iniziale del robot (2 a 14).");
         printf("-x:");
-        scanf("%d",&r.y);
-        printf("-y:");
         scanf("%d",&r.x);
-        if(stanza[r.x][r.y] == ' ' && r.x<size && r.y<size)
+        printf("-y:");
+        scanf("%d",&r.y);
+        if(stanza[r.x][r.y] == ' ' && r.x<n && r.y<m)
             printf("\n-Robot inserito correttamente.\n");
         else
             printf("\n-Errore, riprova.\n");
     }
-    while(stanza[r.x][r.y] == 'X' || (r.x>size && r.y>size));
+    while(stanza[r.x][r.y] == 'X' || (r.x>n && r.y>m));
+    stanza[r.x][r.y]='R';
 }
-void movimento1(char stanza[size][size])
+void movimento1(char stanza[n][m])
 {
-    int n;
+    int nr;
     stanza[r.x][r.y]=' ';
-        n=rand()%4;
-        if(n==0 && stanza[r.x][--r.y]=='X' && *p!=2 && (*p==3 || *p==4)) //Nord 1
+    nr=rand()%4;
+    if(nr==0 && stanza[r.x][(--r.y)]=='X')  //NORD 1
+    {
+        if(r.y++ == '.' && pre.p!=2)
         {
             r.y++;
-            *p=1;
+            pre.p=1;
         }
-        if(n==1 && stanza[r.x][++r.y]=='X' && *p!=1 && (*p==3 || *p==4)) //Sud 2
+    }
+    if(nr==1 && stanza[r.x][(++r.y)]=='X')  //SUD 2
+    {
+        if(r.y-- == '.' && pre.p!=1)
         {
             r.y--;
-            *p=2;
+            pre.p=2;
         }
-        if(n==2 && stanza[--r.x][r.y]=='X' && *p!=4 && (*p==1 || *p==2)) //Ovest 3
+    }
+    if(nr==2 && stanza[(--r.x)][r.y]=='X')  //EST 4
+    {
+        if(r.x++ == '.' && pre.p!=3)
         {
             r.x++;
-            *p=3;
+            pre.p=4;
         }
-        if(n==3 && stanza[++r.x][r.y]=='X' && *p!=3 && (*p==1 || *p==2)) //Est 4
+    }
+    if(nr==3 && stanza[(++r.x)][r.y]=='X')  //OVEST 3
+    {
+        if(r.x-- == '.' && pre.p!=4)
         {
             r.x--;
-            *p=4;
+            pre.p=3;
         }
+    }
+    if((r.x>=0 && r.x<n) && (r.y>=0 && r.y<m))
+        stanza[r.x][r.y]='R';
+}
+
+void mvoimento2(char stanza[n][m])
+{
+
 }
 
