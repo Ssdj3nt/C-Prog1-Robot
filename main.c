7e44e6a7db86c9546884dda6ae84cf45fa29b0b3
Francesco <francesco.porritiello001@studenti.uniparthenope.it>
2022-02-19 20:40:37 +0100

    r

--- main.c
+++ main.c
@@ -94,11 +94,11 @@ void movimento1(char stanza[size][size])
 
     switch(nr)
         {
-            case 0:if(p!=2){c.x++;p=1;}if(stanza[c.x][c.y]=='X'){c.x--;p=0;}break; //Se il robot non é stato precedentemente a SUD allora... - Caso in cui il robot scelga di andare a NORD.
+            case 0:if(p!=2){c.x++;p=1;}if(stanza[c.x][c.y]=='X' && c.x<=size){c.x--;p=0;}break; //Se il robot non é stato precedentemente a SUD allora... - Caso in cui il robot scelga di andare a NORD.
 
-            case 1:if(p!=1){c.x--;p=2;}if(stanza[c.x][c.y]=='X'){c.x++;p=0;}break; //Se il robot non é stato precedentemente a NORD allora... - Caso in cui il robot scelga di andare a SUD.
+            case 1:if(p!=1){c.x--;p=2;}if(stanza[c.x][c.y]=='X' && c.x>=0){c.x++;p=0;}break; //Se il robot non é stato precedentemente a NORD allora... - Caso in cui il robot scelga di andare a SUD.
 
-            case 2:if(p!=4){c.y++;p=3;}if(stanza[c.x][c.y]=='X' && c.y<size){c.y--;p=0;}break; //Se il robot non é stato precedentemente a OVEST allora... - Caso in cui il robot scelga di andare a EST.
+            case 2:if(p!=4){c.y++;p=3;}if(stanza[c.x][c.y]=='X' && c.y<=size){c.y--;p=0;}break; //Se il robot non é stato precedentemente a OVEST allora... - Caso in cui il robot scelga di andare a EST.
 
             case 3:if(p!=3){c.y--;p=4;}if(stanza[c.x][c.y]=='X' && c.y>=0){c.y++;p=0;}break; //Se il robot non é stato precedentemente a EST allora... - Caso in cui il robot scelga di andare a OVEST.
         }
